a04595eabf5ecb2fd56f109640a6cc5d8ff0193f
Ankur Kumar <ankur.kumar@crowe.com>
2025-10-06 16:05:09 +0530

    Contains Invalid Code. Just to test Codex PR freature

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@ function multiplyNumbers(a: number, b: number): number {
     return a * b;
 }
 
-const sumResult = addNumbers(5, 10);
+function multiplyNumbers1(a: number, b: number) {
+    
+}
+
+const sumResult = addNumbers1(5, 10);
 console.log(`The sum of 5 and 10 is: ${sumResult}`);
 
 const productResult = multiplyNumbers(5, 10);
